c86a263498eeac56a2f40fd2ffa58bebf6e4fab4
marekhakala <hakala.marek@gmail.com>
2017-08-31 14:45:39 +0200

    Final commit

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,23 +1,60 @@
 class StocksController < ApplicationController
+  before_action :set_stock, only: [:show, :update, :destroy]
+
   def index
+    @stocks = Stock.all.includes(:market_price, :bearer)
+    render json: @stocks, status: :ok
   end
 
   def show
+    @stock = Stock.where(id: params[:id]).first
+    render json: @stock, status: :ok
   end
-
+ 
   def create
+    unless stock_params.values.include?("invalid")
+      @stock = Stock.new stock_params
+      if @stock.save
+        render json: @stock, status: :created
+      else
+        render json: { errors: @stock.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { errors: filtered_invalid_stock_params }, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @stock.update stock_params
+      render json: @stock, status: :ok
+    else
+      render json: { errors: @stock.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    if @stock and @stock.destroy
+      render json: { message: "Stock deleted" }, status: :ok
+    else
+      render json: { errors: @stock.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   protected
     def set_stock
+      @stock = Stock.find params[:id]
     end
 
     def stock_params
+      params.require(:stock).permit(:name, :bearer_name, :value_cents, :currency)
+    end
+
+    def filtered_invalid_stock_params
+      output_hash_params = {}
+      stock_params.each do |key, value|
+        output_hash_params["#{key}"] = ["is invalid"] if value == "invalid"
+      end
+
+      return output_hash_params
     end
 end
--- spec/controller/stocks_controller_spec.rb
+++ spec/controller/stocks_controller_spec.rb
@@ -2,30 +2,69 @@ require 'rails_helper'
 
 RSpec.describe StocksController, type: :controller do
 
-  describe "GET /stocks/" do
+  let(:stock) { FactoryGirl.create(:stock, :demo) }
+  let(:bearer) { FactoryGirl.create(:bearer, :demo) }
+  let(:bearer_second) { FactoryGirl.create(:bearer, :demo_second) }
+  let(:market_price) { FactoryGirl.create(:market_price, :demo) }
+
+  describe "GET /stocks" do
     it "should return list with stocks" do
+      5.times { |index| FactoryGirl.create(:stock, name: "Stock #{index}",
+       bearer: bearer, market_price: market_price) }
+
+      get :index, as: :json
+      expect(response).to have_http_status(200)
+      json_data = JSON.parse(response.body)
+
+      expect(json_data[0]["name"]).to eq("Stock 0")
+
+      expect(json_data[0]["bearer"]["id"]).to eq(1)
+      expect(json_data[0]["bearer"]["name"]).to eq("Me")
+
+      expect(json_data[0]["market_price"]["id"]).to eq(1)
+      expect(json_data[0]["market_price"]["currency"]).to eq("EUR")
+      expect(json_data[0]["market_price"]["value_cents"]).to eq(1939)
+
+      expect(json_data.count).to eq(5)
     end
   end
 
-  describe "POST /stocks/" do
+  describe "POST /stocks" do
     context "with valid data" do
-      it "should return stock and 200 OK" do
+      it "should return stock and 201 created OK" do
+        stock_params = { stock: { name: "Alphabet", bearer_name: "Greg Snowden",
+           value_cents: 1939, currency: "EUR" } }
+        post :create, as: :json, params: stock_params
+
+        expect(response).to have_http_status(201)
+        json_data = JSON.parse(response.body)
+
+        expect(json_data["name"]).to eq(stock_params[:stock][:name])
+
+        expect(json_data["bearer"]["id"]).to eq(1)
+        expect(json_data["bearer"]["name"]).to eq(stock_params[:stock][:bearer_name])
+
+        expect(json_data["market_price"]["id"]).to eq(1)
+        expect(json_data["market_price"]["currency"]).to eq(stock_params[:stock][:currency])
+        expect(json_data["market_price"]["value_cents"]).to eq(stock_params[:stock][:value_cents])
+
+        expect(Stock.count).to eq(1)
+        expect(Bearer.count).to eq(1)
+        expect(MarketPrice.count).to eq(1)
       end
     end
 
     context "with invalid data" do
       it "should return 422 and fail to save" do
-
-        params = { name: "invalid", bearer: "invalid", value: 19.39, currency: "EUR" }
-
-        get :create, method: :post, params: params
+        stock_params = { stock: { name: "invalid", bearer_name: "invalid", value_cents: 1939, currency: "EUR" } }
+        post :create, method: :post, as: :json, params: stock_params
+        response_body_content = { "errors" => { "name" => ["is invalid"], "bearer_name" => ["is invalid"] }}
 
         expect(response).to have_http_status(422)
-        expect(JSON.parse(response.body)).to eq({"error"=>{"name"=>["is invalid"], "bearer"=>["is invalid"]}})
+        expect(JSON.parse(response.body)).to eq(response_body_content)
         expect(Stock.count).to eq(0)
         expect(Bearer.count).to eq(0)
         expect(MarketPrice.count).to eq(0)
-
       end
     end
   end
@@ -33,16 +72,58 @@ RSpec.describe StocksController, type: :controller do
   describe "PATCH /stocks/:id" do
     context "with different bearer" do
       it "should create new bearer but keep market price" do
+        stock_object = stock
+        stock_params = { stock: { bearer_name: "Test" }, id: stock_object.id }
+        patch :update, as: :json, params: stock_params
+
+        expect(response).to have_http_status(200)
+        json_data = JSON.parse(response.body)
+
+        expect(json_data["name"]).to eq(stock_object.name)
+        expect(json_data["bearer"]["name"]).to eq(stock_params[:stock][:bearer_name])
+        expect(json_data["market_price"]["currency"]).to eq(stock_object.market_price.currency)
+        expect(json_data["market_price"]["value_cents"]).to eq(stock_object.market_price.value_cents)
+
+        expect(Stock.count).to eq(1)
+        expect(Bearer.count).to eq(2)
+        expect(MarketPrice.count).to eq(1)
       end
     end
 
     context "with different market price" do
       it "should create new market price but keep bearer" do
+        stock_object = stock
+        stock_params = { stock: { value_cents: 698, currency: stock_object.market_price.currency }, id: stock_object.id }
+        patch :update, as: :json, params: stock_params
+
+        expect(response).to have_http_status(200)
+        json_data = JSON.parse(response.body)
+
+        expect(json_data["name"]).to eq(stock_object.name)
+        expect(json_data["bearer"]["name"]).to eq(stock_object.bearer.name)
+        expect(json_data["market_price"]["currency"]).to eq(stock_object.market_price.currency)
+        expect(json_data["market_price"]["value_cents"]).to eq(stock_params[:stock][:value_cents])
+
+        expect(Stock.count).to eq(1)
+        expect(Bearer.count).to eq(1)
+        expect(MarketPrice.count).to eq(2)
       end
     end
 
     context "with existing bearer" do
       it "should reference existing bearer to stock" do
+        stock_object = stock
+        bearer_exist = bearer_second
+
+        stock_params = { stock: { bearer_name: bearer_exist.name }, id: stock_object.id }
+        patch :update, as: :json, params: stock_params
+
+        expect(response).to have_http_status(200)
+        expect(Stock.find(1).bearer.name).to eq(bearer_exist.name)
+
+        expect(Stock.count).to eq(1)
+        expect(Bearer.count).to eq(2)
+        expect(MarketPrice.count).to eq(1)
       end
     end
   end
@@ -50,8 +131,14 @@ RSpec.describe StocksController, type: :controller do
   describe "DELETE /stocks/:id" do
     context "with different bearer" do
       it "should create new bearer but keep market price" do
+        stock_object = stock
+        delete :destroy, as: :json, params: { id: stock_object.id }
+
+        expect(response).to have_http_status(200)
+        expect(Stock.count).to eq(0)
+        expect(Bearer.count).to eq(1)
+        expect(MarketPrice.count).to eq(1)
       end
     end
   end
-
 end
